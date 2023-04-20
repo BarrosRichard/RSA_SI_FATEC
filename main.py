edaacb13e07940f1f180f42d5182b69eb915692c
Richard Barros <richardbarros@duck.com>
2023-04-19 23:26:37 -0300

    :sparkles: feat - Nova forma para encontrar o valor de 'D' atravez do calculo para achar o inverso modular

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def is_prime(n) -> bool:
 def generate_prime_number() -> int:
     prime = False
     while not prime:
-        p = random.randint(100, 1000)
+        p = random.randint(1000, 100000)
         if is_prime(p):
             prime = True
     return p
@@ -26,30 +26,6 @@ def gcd(a, b) -> int:
     while b != 0:
         a, b = b, a % b
     return a
-
-def multiplicative_inverse(e, z) -> int:
-    d = 0
-    x1 = 0
-    x2 = 1
-    y1 = 1
-    temp_z = z
-
-    while e > 0:
-        temp1 = temp_z // e
-        temp2 = temp_z - temp1 * e
-        temp_z = e
-        e = temp2
-
-        x = x2 - temp1 * x1
-        y = d - temp1 * y1
-
-        x2 = x1
-        x1 = x
-        d = y1
-        y1 = y
-
-    if temp_z == 1:
-        return d + z
 
 def generate_key_pair() -> tuple:
     p = generate_prime_number()
@@ -63,7 +39,7 @@ def generate_key_pair() -> tuple:
         e = random.randint(1, z)
         g = gcd(e, z)
 
-    d = multiplicative_inverse(e, z)
+    d = pow(e, -1, z)
 
     return ((n, e), (n, d))
 
